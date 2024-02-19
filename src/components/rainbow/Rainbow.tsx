37410197ac9bc9ce25f23369c199e960bf714a40
Bo Robbrecht <robbrechtbo@hotmail.com>
2024-02-19 17:03:15 +0100

    Configured Mantine provider

---
+++ src/components/rainbow/Rainbow.tsx
@@ -0,0 +1,15 @@
+import './Rainbow.css';
+
+export default function Rainbow(){
+    return (
+        <div>
+            <span id='rainbow-red'/>
+            <span id='rainbow-orange'/>
+            <span id='rainbow-yellow'/>
+            <span id='rainbow-green'/>
+            <span id='rainbow-blue'/>
+            <span id='rainbow-indigo'/>
+            <span id='rainbow-violet'/>
+        </div>
+    )
+}
