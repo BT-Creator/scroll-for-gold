173f82e48013c713af3030fef1f80ee8c174c6a4
Bo Robbrecht <robbrechtbo@hotmail.com>
2024-02-19 18:43:23 +0100

    Added reference back to IMD's Useless web

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
+import './Header.module.css'
 import { Group, Text } from "@mantine/core";
 
 export default function Header(){
     return (
         <Group justify="space-between" p="lg" bg="black" wrap="nowrap">
             <Text size="md" fw={700} c="white">Scroll For Gold</Text>
-            <Text size="sm" c="white" >Part of IMD's useless websites</Text>
+            <a href="https://uselessweb.weareimd.be">Part of IMD's useless websites</a>
         </Group>
     );
 }
